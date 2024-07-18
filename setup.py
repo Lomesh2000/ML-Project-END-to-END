b04a50bc0e7900bf708d50f18a609128f797cba9
Lomesh2000 <lomeshsoni70@gmail.com>
2024-07-18 23:06:21 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="project",
+    version="1.0",
+    author='Lomesh',
+    author_email='lomeshsoniwork@gmail.com',
+    packages=find_packages()
+)
