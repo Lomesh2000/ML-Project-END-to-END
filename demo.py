71642e3cf0cad660635d2a82f4ab4cc044b61abd
Lomesh2000 <lomeshsoni70@gmail.com>
2024-07-22 14:23:20 +0530

    custom exception

--- demo.py
+++ demo.py
@@ -1,3 +1,9 @@
 from project.logger import logging
+from project.exception import CustomException
+import sys
 
 logging.info('Welcome')
+try:
+    a = 10/0
+except Exception as e:
+    raise CustomException(e, sys)    
