46d48d2f3944d3561c5e305820b7b96aae2dc979
Lomesh2000 <lomeshsoni70@gmail.com>
2024-08-04 00:22:49 +0530

    added data validation in pipeline

--- project/pipeline/training_pipeline.py
+++ project/pipeline/training_pipeline.py
@@ -4,22 +4,36 @@ from project.exception import CustomException
 from project.logger import logging
 
 from project.components.data_ingestion import DataIngestion
-from project.entity.config_entity import DataIngestionConfig
-from project.entity.artifact_entity import DataIngestionArtifacts
+from project.components.data_validation import DataValidation
+from project.entity.config_entity import DataIngestionConfig, DataValidationConfig
+from project.entity.artifact_entity import DataIngestionArtifacts, DataValidationArtifacts
 
 class TrainingPipeline:
 
     def __init__(self):
         self.data_ingestion_config = DataIngestionConfig()
+        self.data_validation_config = DataValidationConfig()
 
     def start_data_ingestion(self) -> DataIngestionArtifacts:
         data_ingestion = DataIngestion(data_ingestion_config=self.data_ingestion_config)
         data_ingestion_artifacts = data_ingestion.initiate_data_ingestion()
         logging.info('got the data ingestion artifacts')
         return data_ingestion_artifacts
+    
+    def start_data_validtion(self, data_ingestion_artifact) -> DataValidationArtifacts:
+        logging.info('starting data validation')
+
+        data_validation = DataValidation(data_ingestion_artifact=data_ingestion_artifact,
+                                         data_validation_config=self.data_validation_config)
+        data_validation_artifacts = data_validation.initiate_data_validation()
+        logging.info('data validation done')
+
+        return data_validation_artifacts
 
     def run_pipeline(self):
         try:
             data_ingestion_artifacts = self.start_data_ingestion() 
+            data_validation_artifacts = self.start_data_validtion(data_ingestion_artifact=data_ingestion_artifacts)
+        
         except Exception as e:
             raise CustomException(e, sys) from e
