d1a5a63cbd4f463bce832644dd586f9a0d50798c
Lomesh2000 <lomeshsoni70@gmail.com>
2024-08-03 15:14:12 +0530

    convert data from mongdb to dataframe

---
+++ project/data_access/convert_mongo_dict_to_df.py
@@ -0,0 +1,36 @@
+from project.configuration.mongodb_connection import MongoDBClient
+from project.constants import DB_NAME
+from project.logger import logging
+from project.exception import CustomException
+import sys
+import pandas as pd
+import numpy as np
+from typing import Optional
+
+class DataFrameFromMongoDB:
+    """
+    this class convert data from mongo db into dataframe from dict 
+    """
+
+    def __init__(self) -> None:
+        try:
+            self.mongo_client = MongoDBClient(database_name=DB_NAME)
+        except Exception as e:
+            raise CustomException(e, sys)
+
+    def export_collection_as_data_frame(self, collection_name: str, database_name:Optional[str] = None) -> pd.DataFrame:
+        try:
+            if not database_name:
+                collection = self.mongo_client.database[collection_name]
+            else:
+                collection = self.mongo_client[database_name][collection_name]
+
+            df = pd.DataFrame(list(collection.find()))
+            if '_id' in df.columns:
+                df.drop(columns=['_id'], axis=1, inplace=True)
+            df.replace({'na':np.nan}, inplace=True)
+            logging.info(f'collection from database : {database_name} is converted to DATA FRAME')    
+            return df   
+        except Exception as e:
+            raise CustomException(e, sys)
+        
